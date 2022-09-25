db8d52f2c41f859be1a2e7a2dba1ea4dfc2acb3c
Anasity <nastya.dubovik.2022@mail.ru>
2022-09-25 22:36:32 +0500

    Правка кода
    Полное изменение кода

--- Лаб.03_Дубовик/Шифр_Цезаря.py
+++ Лаб.03_Дубовик/Шифр_Цезаря.py
@@ -1,23 +1,31 @@
-alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-step = int(input('Введите шаг шифровки: '))
-text = input('Введите сообщение: ').upper()
-n = ''
+def func_caesar_ru(message, step): 
+    result = "" 
+    for i in range(len(message)): 
+        char = message[i] 
+        if(char.isupper()): 
+            result += chr((ord(char) + step - 1040) % 32 + 1040) 
+        else: 
+            result += chr((ord(char) + step - 1072) % 32 + 1072) 
+    return result 
+def func_caesar_eu(message, step): 
+    result = "" 
+    for i in range(len(message)): 
+        char = message[i] 
+        if(char.isupper()): 
+            result += chr((ord(char) + step - 65) % 26 + 65) 
+        else: 
+            result += chr((ord(char) + step - 97) % 26 + 97) 
+    return result 
+    
+message = input("Сообщение для ДЕшифровки: ").upper() 
+step = int(input('Шаг шифровки: '))  
 lang = input('Выберите язык RU/EU: ')
+
+print("Сообщение: " + message) 
+print("Шаг: " + str(step)) 
 if lang == 'RU':
-    for i in text:
-        mesto = alfavit_RU.find(i)
-        mesto_1 = mesto + step
-        if i in alfavit_RU:
-            n += alfavit_RU[mesto_1]
-        else:
-            n += i
+    print("Резултат: " + func_caesar_ru(message, step)) 
+elif lang == 'EU':
+   print("Резултат: " + func_caesar_eu(message, step))  
 else:
-    for i in text:
-        mesto = alfavit_EU.find(i)
-        mesto_1 = mesto + step
-        if i in alfavit_EU:
-            n += alfavit_EU[mesto_1]
-        else:
-            n += i
-print (n)
+    print("Неверно выбран язык") 
