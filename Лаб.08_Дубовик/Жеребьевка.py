831324311d63ef891d6096701ae3e5dfb76d90a1
Anasity <nastya.dubovik.2022@mail.ru>
2022-11-13 21:08:29 +0500

    Лаба 8
    Код, README, скрины

---
+++ Лаб.08_Дубовик/Жеребьевка.py
@@ -0,0 +1,37 @@
+import random
+import keyboard
+
+
+def number(tn): #функция для обрабоки ошибок ввода координат
+    try:
+        tn = int(tn) #преобразование в целое число
+        if tn>=1:
+            return tn
+        else:
+            print('Вы не ввели количество бочек: ')
+            return 'False'
+    except:
+        print('Вы не ввели количество бочек: ')
+        return 'False'
+
+N = input('введите количество бочек: ')
+while number(N) == 'False': #обработка ошибок ввода N
+    N = input('введите количество бочек: ')
+N = number(N)
+print('для проведения жеребьевки нажимайте пробел')
+A = random.sample(range(1,N+1), N) #функция возвращает
+# случайные элементы из списка без повторений
+
+for i in A:
+    print(f'случайное число - {i}')
+    while 1:
+        keyboard.wait(' ')
+        if keyboard.is_pressed(' '): #функция принимает символ в качестве
+                                     #входных данных и возвращает True
+            break
+        else:
+            continue
+
+
+
+
